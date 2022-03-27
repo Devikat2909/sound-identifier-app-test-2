52fe5e0f9dc02da6c2f584be9a27b622a7bbf45f
Devikat2909 <87276536+Devikat2909@users.noreply.github.com>
2022-03-27 19:37:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//https://teachablemachine.withgoogle.com/models/fgBoFsqew/
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('//https://teachablemachine.withgoogle.com/models/fgBoFsqew/model.json', modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results)
+{
+    console.log("gotResult")
+}
